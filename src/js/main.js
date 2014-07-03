8645b969b3b0178187cee50eccfe13548b2610ca
Mark Durrant <mark.j.durrant.gmail.com>
2014-07-03 13:50:36 +0100

    things

--- src/js/main.js
+++ src/js/main.js
@@ -6,12 +6,26 @@
     elm.css( 'background', elm.attr( 'data-shortcode' ) );
   }
 
-  // set button colors
+  // set text to data-shortcode attribute
+  function printShortCode( elm ) {
+    elm.children().text( elm.attr( 'data-shortcode' ).substring( 1 ) );
+  }
+
+  // set button colors & text
   $( 'button' ).each( function() {
     paintShortCode( $( this ) );
+    printShortCode( $( this ) );
   } );
 
   // set current code color
   paintShortCode( $( '.vals-container' ) );
+
+  // get currect shortcode
+  var currentCode = $( '.vals-container' ).attr( 'data-shortcode' );
+
+  // set currect code text
+  $( '.R-val span' ).text( currentCode.charAt( 1 ) );
+  $( '.G-val span' ).text( currentCode.charAt( 2 ) );
+  $( '.B-val span' ).text( currentCode.charAt( 3 ) );
 
 }() ); // end 'use strict'
