16a2b15a97457d5d7921280c7518d55795ed3484
Mark Durrant <mark.j.durrant.gmail.com>
2014-07-03 12:06:05 +0100

    map data-attr to backgrond colors

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,17 @@
-(function () { 'use strict';
+( function() {
+  'use strict';
 
-}()); // end 'use strict'
+  // set background to data-shortcode attribute
+  function paintShortCode( elm ) {
+    elm.css( 'background', elm.attr( 'data-shortcode' ) );
+  }
+
+  // set button colors
+  $( 'button' ).each( function() {
+    paintShortCode( $( this ) );
+  } );
+
+  // set current code color
+  paintShortCode( $( '.vals-container' ) );
+
+}() ); // end 'use strict'
