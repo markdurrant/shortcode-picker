36461aa45a465e4af96cb11b502a2d5f8de806cf
Mark Durrant <mark.j.durrant.gmail.com>
2014-07-14 16:23:18 +0100

    copy hsltorgb

--- src/js/colorFunctions.js
+++ src/js/colorFunctions.js
@@ -1,53 +1,10 @@
 ( function() {
   'use strict';
-  /**
-   * Converts an RGB color value to HSL. Conversion formula
-   * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
-   * Assumes r, g, and b are contained in the set [0, 255] and
-   * returns h, s, and l in the set [0, 1].
-   *
-   * @param   Number  r       The red color value
-   * @param   Number  g       The green color value
-   * @param   Number  b       The blue color value
-   * @return  Array           The HSL representation
-   */
-  function rgbToHsl( r, g, b ) {
-    r /= 255;
-    g /= 255;
-    b /= 255;
-    var max = Math.max( r, g, b ),
-      min = Math.min( r, g, b );
-
-    var h, s, l = ( max + min ) / 2;
-
-    if ( max == min ) {
-      h = s = 0; // achromatic
-    } else {
-      var d = max - min;
-      s = l > 0.5 ? d / ( 2 - max - min ) : d / ( max + min );
-      switch ( max ) {
-        case r:
-          h = ( g - b ) / d + ( g < b ? 6 : 0 );
-          break;
-        case g:
-          h = ( b - r ) / d + 2;
-          break;
-        case b:
-          h = ( r - g ) / d + 4;
-          break;
-      }
-      h /= 6;
-    }
-
-    return [ h, s, l ];
-  }
-
-  var example = [ 123, 45, 225 ];
 
   // my rgb to hsl. Based on http://axonflux.com/handy-rgb-to-hsl-and-rgb-to-hsv-color-model-c
   // input rgb array [ r, g, b ] r,g,b 0-255
   // output hsl array [ h, s, l ] h 0-360 | s,l 0-100
-  function myHsl( array ) {
+  function rgbToHsl( array ) {
     // get r,g, & b values and normalise to 0 - 1
     var r = array[ 0 ] / 255,
       g = array[ 1 ] / 255,
@@ -100,9 +57,54 @@
       s = 0;
     }
 
-    return [ "RGB", r * 255, g * 255, b * 255, "HSL", h, s, l ];
+    return [ h, s, l ];
   }
 
-  console.log( myHsl( example ) );
+  // my hsl to rgb. Based on http://axonflux.com/handy-rgb-to-hsl-and-rgb-to-hsv-color-model-c
+  // input hsl array [ h, s, l ] h 0-360 | s,l 0-100
+  // output rgb array [ r, g, b ] r,g,b 0-255
+  function hslToRgb( array ) {
+    var h = array[ 0 ] / 360;
+    var s = array[ 1 ] / 100;
+    var l = array[ 2 ] / 100;
+
+    function hue2rgb( p, q, t ) {
+      if ( t < 0 ) t += 1;
+      if ( t > 1 ) t -= 1;
+      if ( t < 1 / 6 ) return p + ( q - p ) * 6 * t;
+      if ( t < 1 / 2 ) return q;
+      if ( t < 2 / 3 ) return p + ( q - p ) * ( 2 / 3 - t ) * 6;
+      return p;
+    }
+
+    var r, g, b;
+
+    if ( s === 0 ) {
+      r = g = b = l; // achromatic
+    } else {
+      var q = l < 0.5 ? l * ( 1 + s ) : l + s - l * s;
+      var p = 2 * l - q;
+      r = hue2rgb( p, q, h + 1 / 3 );
+      g = hue2rgb( p, q, h );
+      b = hue2rgb( p, q, h - 1 / 3 );
+    }
+
+    r = Math.round( r * 255 );
+    g = Math.round( g * 255 );
+    b = Math.round( b * 255 );
+
+    return [ r, g, b ];
+  }
+
+  // convert rgb to shortcode
+  function rgbToShortCode( array ) {
+
+  }
+
+  var example = [ 123, 45, 225 ];
+
+  console.log( "RGB", example );
+  console.log( "HSL", rgbToHsl( example ) );
+  console.log( "RGB", hslToRgb( [ 266, 75, 53 ] ) );
 
 }() ); // end 'use strict'
